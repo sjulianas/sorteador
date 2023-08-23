c2a490f7b6d6b5829476ee05a8adbad4c5793dca
sjulianas <142349481+sjulianas@users.noreply.github.com>
2023-08-23 14:31:35 -0300

    sorteador.py

--- sorteador.py
+++ sorteador.py
@@ -6,20 +6,22 @@ resto = 0
 while resto == 0:
     num_sor = random.randrange(0, 100) #sorteia de 0 á 100
     resto = num_sor % 2 #define que é impar (dividindo por 2)
-    print(num_sor, resto)
+    print(" ♥ SORTEADOR - - - ADIVINHE O NÚMERO SORTEADO!")
+    print("Você tem:",vida,"DE VIDA")
 
 while tent > 0 and vida > 0: #enquanto a tentativa e vida é maior que zero... (para rodar)
     number = int(input ("Digite um número: ")) #Usuário define o numero que será guardado (criação da variavel escre_number)
     if number == num_sor:
-        print("Você venceu")   #if é tipo o acerto, se não...
+        print("UHHHHHHUL Você venceu ♥☺")   #if é tipo o acerto, se não...
         break
     else:                      #se não...
         tent = tent - 1        # número de tentativas
         vida = vida - abs(number - num_sor)   #Número de vidas vida(número escrito - número sorteado)
-        print("Você errou!")
+        print("Você errou! poxa que pena, tente novamente ")
+        print("Você tem:", vida, "DE VIDA")
         continue
 if tent == 0 or vida <= 0:    #quando a tentativa for zero ou a vida menor ou igual a zero acaba o jogo
     print("Poxa camarada se perdeu ermão")
-
+    print("o número sorteado era: ", num_sor)
 
 
